5938a2716ac7337584342f65767f54225fe12c75
Priyanshi248 <ps19032414@gmail.com>
2026-09-19 23:36:30 +0530

    Allow production frontend CORS

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://greencloud-optimizer.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
